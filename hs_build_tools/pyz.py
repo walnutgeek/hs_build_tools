4f0633b49d5a8e32af865cf585ce184a598421a4
Walunt Geek <wg@walnutgeek.com>
2019-10-14 14:03:06 -0700

    add more flexibility for pyz task

--- hs_build_tools/pyz.py
+++ hs_build_tools/pyz.py
@@ -10,14 +10,22 @@ class PyzCommand(Command):
     description = f"""
             Build runtime pyz executable using shiv.
             """
-    DEFAULT_DIR = join("dist", "site_packages_for_pyz")
+    DEFAULT_DIR = "./dist"
+    DEFAULT_EP = "hashkernel.cli:main"
 
     user_options = [
-        ("dir=", None, f"where to create runenv, default is: {DEFAULT_DIR}")
+        ("dir=", None, f"where to create .pyz file, default is: {DEFAULT_DIR}"),
+        ("name=", None, f"name of .pyz file, if omitted defaults to package name"),
+        ("script=", None, f"entry point used in .pyz file created, default is {DEFAULT_EP}")
     ]
 
     def initialize_options(self):
         self.dir = self.DEFAULT_DIR
+        self.script = self.DEFAULT_EP
+        self.name = f'{self.distribution.get_name()}.pyz'
+
+    def target_dir(self):
+        return join(self.dir, "site_packages_for_pyz")
 
     def finalize_options(self):
         pass
@@ -28,20 +36,24 @@ class PyzCommand(Command):
         """
         if not exists("setup.py"):
             raise ValueError(f"Has to be run from root of the project")
-        rmtree(self.dir, ignore_errors=True)
+        rmtree(self.target_dir(), ignore_errors=True)
+        makedirs(self.target_dir(), exist_ok=True)
+        check_call(["pip", "install", ".", "--target", self.target_dir()])
         makedirs(self.dir, exist_ok=True)
-        check_call(["pip", "install", ".", "--target", self.dir])
-        makedirs("dist", exist_ok=True)
-        for ep in self.distribution.entry_points["console_scripts"]:
-            ep_name, ep_method = map(lambda s: s.strip(), ep.split("="))
-            check_call(
-                [
-                    "shiv",
-                    "--site-packages",
-                    self.dir,
-                    "-o",
-                    f"dist/{ep_name}.pyz",
-                    "-e",
-                    ep_method,
-                ]
-            )
+        self.run_shiv(self.name, self.script)
+
+    def run_shiv(self, ep_name, ep_method):
+        pyz_name = join(self.dir, self.name)
+        check_call(
+            [
+                "shiv",
+                "--site-packages",
+                self.target_dir(),
+                "-o",
+                pyz_name,
+                "-e",
+                ep_method,
+            ]
+        )
+        print(f"Created: {pyz_name} script entry point: {ep_method}")
+
